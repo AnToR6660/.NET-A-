a1509e99c67c9c64303c14fc9704890131e6a538
tahmeed hossain <tahmeedhossain99@gmail.com>
2024-02-05 00:34:23 +0600

    dynamically value updated

---
+++ HW1/HW1/Models/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1.Models
+{
+    public class MyClass
+    {
+        public string name;
+        public string id; 
+        public string email;  
+
+    }
+    public class MyResult
+    {
+        public string degree;
+        public ushort year;
+        public string institution;
+
+    }
+
+    public class PersonalDetails 
+    {
+        public string name;
+        public string age;
+        public string email;
+        public string phone;
+        public string position;
+    }
+    public class Project 
+    {
+        public string ProjectName;
+        public string ProjectDetails;
+    }
+
+    public class Referance
+    {
+        public string name;
+        public string email;
+        public string contact;
+    }
+}
